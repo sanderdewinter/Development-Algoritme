9cded22dea942b857c4086d9062fbfcc41e5b961
Sander de Winter <sanderdewinter@gmail.com>
2015-01-08 10:29:46 +0100

    Start up, class aangemaakt en gitignore

--- src/Bestelling.java
+++ src/Bestelling.java
@@ -1,5 +1,24 @@
+import java.security.Timestamp;
+
 /**
  * Created by sander on 1/8/15.
  */
 public class Bestelling {
+
+    int klantID, bestellingID;
+    boolean verwerking;
+    Timestamp startTijd;
+    Timestamp duur;
+    boolean compleet;
+    boolean dadelijk;
+
+    public Bestelling(int klantID, int bestellingID, boolean verwerking, Timestamp startTijd, Timestamp duur, boolean compleet, boolean dadelijk) {
+        this.klantID = klantID;
+        this.bestellingID = bestellingID;
+        this.verwerking = verwerking;
+        this.startTijd = startTijd;
+        this.duur = duur;
+        this.compleet = compleet;
+        this.dadelijk = dadelijk;
+    }
 }
--- src/Klant.java
+++ src/Klant.java
@@ -2,4 +2,22 @@
  * Created by sander on 1/8/15.
  */
 public class Klant {
+
+    int klantID;
+    String achternaam, tussenvoegsel, voornaam;
+    int leeftijd;
+    char geslacht;
+    String plaats;
+    String emailAdres;
+
+    public Klant(int klantID, String achternaam, String tussenvoegsel, String voornaam, int leeftijd, char geslacht, String plaats, String emailAdres) {
+        this.klantID = klantID;
+        this.achternaam = achternaam;
+        this.tussenvoegsel = tussenvoegsel;
+        this.voornaam = voornaam;
+        this.leeftijd = leeftijd;
+        this.geslacht = geslacht;
+        this.plaats = plaats;
+        this.emailAdres = emailAdres;
+    }
 }
